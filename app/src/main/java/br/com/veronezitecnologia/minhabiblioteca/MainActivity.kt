e3462b144a7e7d8c405a2b322fc3a94e524d8b1d
jafferveronezi <jafferveronezi@gmail.com>
2018-10-16 22:29:40 -0300

    criacao do toast customizado

--- app/src/main/java/br/com/veronezitecnologia/minhabiblioteca/MainActivity.kt
+++ app/src/main/java/br/com/veronezitecnologia/minhabiblioteca/MainActivity.kt
@@ -2,11 +2,15 @@ package br.com.veronezitecnologia.minhabiblioteca
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.veronezitecnologia.minhabiblioteca.demo.Calculadora
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calculadora = Calculadora()
+        calculadora.somar(1, 2)
     }
 }
